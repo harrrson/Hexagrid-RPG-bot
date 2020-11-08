d81a0708a99d03a975cdfc5878729ecd2e488f0a
harrrson <petrakus03+git@gmail.com>
2020-11-08 21:00:29 +0100

    Removing unnecessary line in sh script

--- bot_start.sh
+++ bot_start.sh
@@ -4,4 +4,3 @@ echo "$SCRIPT"
 DIR=$(dirname -- "$SCRIPT")
 echo "${DIR}"
 screen -dSm bot -c "$DIR/.screenrc"
-screen -S bot -X bash "source '$DIR/venv/bin/activate'&& cd '$DIR' && python Hexagrid_RPG_bot.py"
